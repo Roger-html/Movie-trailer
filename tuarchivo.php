3733e6dac87c7d89e72abf4279e535a8c676dace
root <root@localhost>
2020-06-05 12:00:11 -0400

    publishing awesome pages 1591372811276

--- tuarchivo.php
+++ tuarchivo.php
@@ -1,3 +1,12 @@
 <?php 
 echo "Gracias tu mensaje pronto sera respondido";
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+echo $nombre;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $message;
 ?>
